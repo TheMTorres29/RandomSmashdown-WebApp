e87a28156b14e222e6b92a04e343ed071cee37f0
TheMTorres29 <39773958+TheMTorres29@users.noreply.github.com>
2023-12-20 22:17:11 -0800

    RandomFunctionality and CSS

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <p className="footer-text">created by: Miguel Torres</p>
+    </div>
+  )
+}
+
+export default Footer
