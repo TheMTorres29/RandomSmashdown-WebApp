766804e7e08f8096ce0769c677d8267297e13c21
TheMTorres29 <39773958+TheMTorres29@users.noreply.github.com>
2023-12-28 01:47:29 -0800

    Sticky TitleBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import './App.css'
 import SmashRoster from './components/SmashRoster/SmashRoster'
 import Footer from './components/Footer/Footer'
+import TitleBar from './components/TitleBar/TitleBar'
 
 function App() {
 
   return (
     <>
-      <div className='app-title-container'>
-        <div className='title-bar'>
-          <h1 className='app-title'>Random-Smashdown.site</h1>
-        </div>
-      </div>
+      <TitleBar />
       <SmashRoster />
       <Footer />
     </>
---
+++ src/components/TitleBar/TitleBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './TitleBar.css'
+
+const TitleBar = () => {
+  return (
+    <div className='app-title-container'>
+        <div className='title-bar'>
+            <h1 className='app-title'>Random-Smashdown.site</h1>
+        </div>
+    </div>
+  )
+}
+
+export default TitleBar
